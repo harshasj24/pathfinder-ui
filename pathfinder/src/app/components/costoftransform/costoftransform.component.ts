423aa43ee930a114f8e82c220ebbdb533760d171
sahanapatil15 <sahana.patil@collabera.com>
2022-10-10 13:44:07 +0530

    added cost of transformation services

--- pathfinder/src/app/components/costoftransform/costoftransform.component.ts
+++ pathfinder/src/app/components/costoftransform/costoftransform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-costoftransform',
@@ -8,11 +9,17 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CostoftransformComponent implements OnInit {
 
-  constructor() { }
+  constructor( private apiservice:ApiService) { }
+  isLoaded: boolean = false;
+  costData: any;
+  costDataYear:any
 COT = new FormGroup({
     cot: new FormControl('',Validators.required),
     clientshare: new FormControl('',Validators.required),
     partnershare: new FormControl('',Validators.required),
+    cosy1:new FormControl('',Validators.required),
+    cosy2:new FormControl('',Validators.required),
+    cosy3:new FormControl('',Validators.required),
   });
   get cot() {
     return this.COT.get('cot');
@@ -23,8 +30,24 @@ COT = new FormGroup({
   get partnershare() {
     return this.COT.get('partnershare');
   }
+  get cosy1(){
+    return this.COT.get('cosy1')
+  }
+  get cosy2(){
+    return this.COT.get('cosy2')
+  }
+  get cosy3(){
+    return this.COT.get('cosy3')
+  }
   handleSubmit(){
-
+    this.isLoaded = true;
+    this.apiservice.getcostoftransform().subscribe((val)=>{
+      this.costData=val;
+      console.log(this.costData);
+      console.log(this.costData.yearBaseCostCalculations);
+      this.costDataYear=this.costData.yearBaseCostCalculations;
+      console.log(this.costDataYear);
+    })
   }
   ngOnInit(): void {
   }
