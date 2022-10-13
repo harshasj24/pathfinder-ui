9fb15dbc9194d066bf1de3e93127bee4538befb3
BRD\siddik.shaikh <siddik.shaikh@collabera.com>
2022-10-14 00:26:47 +0530

    changes made in the modal ts file

--- pathfinder/src/app/modal-options/modal-options.module.ts
+++ pathfinder/src/app/modal-options/modal-options.module.ts
@@ -5,41 +5,34 @@ import { HomeModalOptionsComponent } from './home-modal-options.component';
 import { SavingmodalBComponent } from './components/savingmodal-b/savingmodal-b.component';
 import { MaterialModule } from '../material';
 import { OpexModalComponent } from './components/opex-modal/opex-modal.component';
-<<<<<<< HEAD
 import { SharedModule } from '../shared/shared.module';
-=======
 import { SavingmodelAComponent } from './components/savingmodel-a/savingmodel-a.component';
 import { RunOpexModelComponent } from './components/run-opex-model/run-opex-model.component';
 import { WaterfallsTabelComponent } from './components/waterfalls-tabel/waterfalls-tabel.component';
->>>>>>> 00789080a5a6847ce818b5decefe5448f9e9e04f
 
 const routes: Routes = [
   {
     path: '',
     component: HomeModalOptionsComponent,
   },
-  // {
-  //   path: 'savingmodal-b',
-  //   component: SavingmodalBComponent,
-  // },
+ 
 ];
 
 @NgModule({
-<<<<<<< HEAD
+  
   declarations: [
     HomeModalOptionsComponent,
     SavingmodalBComponent,
     OpexModalComponent,
+    SavingmodelAComponent,
+    RunOpexModelComponent,
+    WaterfallsTabelComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
     MaterialModule,
+    RouterModule.forChild(routes),
     SharedModule,
   ],
-=======
-  declarations: [ HomeModalOptionsComponent,SavingmodalBComponent,OpexModalComponent,SavingmodelAComponent,RunOpexModelComponent, WaterfallsTabelComponent],
-  imports: [CommonModule,MaterialModule, RouterModule.forChild(routes)],
->>>>>>> 00789080a5a6847ce818b5decefe5448f9e9e04f
 })
 export class ModalOptionsModule {}
