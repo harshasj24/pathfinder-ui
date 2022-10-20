3b709475e9bad523b41a0086a415250b26a2df35
harshasj24 <harshasj24@gmail.com>
2022-10-20 18:02:05 +0530

    updated responcive

--- pathfinder/src/app/core/services/core.service.ts
+++ pathfinder/src/app/core/services/core.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ChartsComponent } from 'src/app/shared/charts/charts.component';
+import { benchmark } from '../constants/benchmarks';
 
 @Injectable()
 export class CoreServices {
   constructor(private dailog: MatDialog) {}
-
+  benchmarks: any = benchmark;
   openDailog(component: any) {
     this.dailog.open(component);
   }
+  setBanchmark(data: any) {}
   openChartsDailog() {
     this.openDailog(ChartsComponent);
   }
